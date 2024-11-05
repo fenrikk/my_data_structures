a22b76e38dee895e4a175561b038b209535a9b5c
Nikita Stukalov <nikita.stukalov07@gmail.com>
2024-11-05 23:47:32 +0000

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,185 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    data: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+pub struct LinkedList<T> {
+    head: Link<T>,
+    tail: Link<T>,
+    length: usize,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        let new_node = Rc::new(RefCell::new(Node {
+            data,
+            next: self.head.clone(),
+            prev: None,
+        }));
+
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_node.clone());
+                self.head = Some(new_node);
+            }
+            None => {
+                self.tail = Some(new_node.clone());
+                self.head = Some(new_node);
+            }
+        }
+        self.length += 1;
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            self.length -= 1;
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev = None;
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail = None;
+                }
+            }
+            Rc::try_unwrap(old_head)
+                .ok()
+                .expect("Something went wrong")
+                .into_inner()
+                .data
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
+    }
+}
+
+pub struct Stack<T> {
+    items: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { items: Vec::new() }
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.items.last()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn size(&self) -> usize {
+        self.items.len()
+    }
+}
+
+pub struct Queue<T> {
+    items: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Self {
+        Queue { items: Vec::new() }
+    }
+
+    pub fn enqueue(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    pub fn dequeue(&mut self) -> Option<T> {
+        if !self.items.is_empty() {
+            Some(self.items.remove(0))
+        } else {
+            None
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.items.first()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn size(&self) -> usize {
+        self.items.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_linked_list() {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_stack() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assert_eq!(stack.peek(), Some(&3));
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.size(), 1);
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
+        assert!(stack.is_empty());
+    }
+
+    #[test]
+    fn test_queue() {
+        let mut queue = Queue::new();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        assert_eq!(queue.peek(), Some(&1));
+        assert_eq!(queue.dequeue(), Some(1));
+        assert_eq!(queue.dequeue(), Some(2));
+        assert_eq!(queue.size(), 1);
+        assert_eq!(queue.dequeue(), Some(3));
+        assert_eq!(queue.dequeue(), None);
+        assert!(queue.is_empty());
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod lib;
+use lib::{LinkedList, Stack, Queue};
+
+fn main() {
+    println!("=== Testing Doubly Linked List ===");
+    let mut list = LinkedList::new();
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    println!("List size: {}", list.len());
+    println!("Extracting elements:");
+    while let Some(item) = list.pop_front() {
+        println!("Got: {}", item);
+    }
+
+    println!("\n=== Testing Stack ===");
+    let mut stack = Stack::new();
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    println!("Stack size: {}", stack.size());
+    println!("Top element: {:?}", stack.peek());
+    println!("Extracting elements:");
+    while let Some(item) = stack.pop() {
+        println!("Got: {}", item);
+    }
+
+    println!("\n=== Testing Queue ===");
+    let mut queue = Queue::new();
+    queue.enqueue(100);
+    queue.enqueue(200);
+    queue.enqueue(300);
+    println!("Queue size: {}", queue.size());
+    println!("First element: {:?}", queue.peek());
+    println!("Extracting elements:");
+    while let Some(item) = queue.dequeue() {
+        println!("Got: {}", item);
+    }
+}
